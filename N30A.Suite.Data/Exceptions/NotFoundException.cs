53e1705d440266f3e5063d7bcad259b123a03fcb
= <neo.ahlander@outlook.com>
2025-06-21 19:18:59 +0200

    Add constructor without message

--- N30A.Suite.Data/Exceptions/NotFoundException.cs
+++ N30A.Suite.Data/Exceptions/NotFoundException.cs
@@ -2,5 +2,6 @@ namespace N30A.Suite.Data.Exceptions;
 
 public class NotFoundException : Exception
 {
+    public NotFoundException() { }
     public NotFoundException(string message) : base(message) { }
 }
